823f69eefd7b201545dee71fa1231eee26f4a256
braden <haskiezgaming@gmail.com>
2018-02-20 16:33:03 -0700

    Add model to game board to set up test case for battleship. Add hit and miss functionality.

--- game-grid.js
+++ game-grid.js
@@ -3,12 +3,56 @@ document.onreadystatechange = function() {
         // create two table elements that go in table-div element
         var tableDiv = document.getElementById("table-div");
         tableDiv.innerHTML = "<table id='player-board'><thead></thead><tbody></tbody></table><table id='computer-board'><thead></thead><tbody></tbody></table>";
-        // createPlayerGrid(11, 11);
-        // createComputerGrid(11, 11);
+        playerGrid = document.getElementById("player-board");
+        computerGrid = document.getElementById("computer-board");
+        // generate the game grids for each player
         generateGrid(11, 11, "player-board");
         generateGrid(11, 11, "computer-board");
+
+        showShips("player-board", placeShipsPlayer());
+        showShips("computer-board", placeShipsComputer());
+
+
+        // add on click event to all hit boxes in both tables
+        var cells = document.getElementsByClassName("hit-box");
+        for (var i = 0; i < cells.length; ++i) {
+            cells[i].addEventListener("click", function() {
+                if (turn == 1) {
+                    if (this.getAttribute("data-board-id") != 1) {
+                        var cellNumStr = this.getAttribute("data-cell-number");
+                        var row = cellNumStr[0];
+                        var col = cellNumStr[2];
+                        var goodShot = shot(1, row, col);
+                        if (goodShot == 1) {
+                            alert("HIT");
+                            var box = computerGrid.querySelector('[data-cell-number="' + row + ',' + col + '"]');
+                            var icon = box.getElementsByTagName("i")[0];
+                            icon.classList.remove("fa-circle-thin");
+                            icon.classList.add("fa-circle");
+                            box.classList.add("hit");
+                        }
+                        else if (goodShot == 0) {
+                            alert("MISS");
+                            var box = computerGrid.querySelector('[data-cell-number="' + row + ',' + col + '"]');
+                            var icon = box.getElementsByTagName("i")[0];
+                            icon.classList.remove("fa-circle-thin");
+                            icon.classList.add("fa-circle");
+                            box.classList.add("miss");
+                        }
+                    }
+                    else {
+                        alert("That is not the right board.");
+                    }
+                }
+                else {
+                    alert("It's not your turn.");
+                }
+            });
+        }
     }
 }
+
+// FUNCTIONS FOR VIEW
 
 // generate the game grid
 // requires the number of rows and columns along with which table you are creating. 
@@ -40,8 +84,31 @@ function generateGrid(rows, columns, tableName) {
                     lastTr.innerHTML += "<td class='row-number'>" + i + "</td>"
                 }
                 else {
-                    lastTr.innerHTML += "<td class='hit-box'><i class='fa fa-circle-thin'></i></td>";
+                    var id;
+                    if (tableName == 'player-board') {
+                        id = 1;
+                        lastTr.innerHTML += "<td class='hit-box' data-cell-number='" + (i - 1) + "," + (j - 1) +"' data-board-id='" + id + "'><i class='fa fa-circle-thin'></i></td>";
+                    }
+                    else {
+                        id = 2;
+                        lastTr.innerHTML += "<td class='hit-box' data-cell-number='" + (i - 1) + "," + j +"' data-board-id='" + id + "'><i class='fa fa-circle-thin'></i></td>";
+                    }
                 }
+            }
+        }
+    }
+}
+
+function showShips(tableName, grid) {
+    var table = document.getElementById(tableName);
+    for (var i = 0; i < grid.length; i++) {
+        var row = grid[i];
+        for (var j = 0; j < row.length; j++) {
+            if (row[j] != 0) {
+                // show a ship in that table cell
+                var searchString = 'data-cell-number="' + i + ',' + j + '"';
+                var box = table.querySelector("[" + searchString + "]");
+                box.classList.add("ship");
             }
         }
     }
---
+++ model.js
@@ -0,0 +1,438 @@
+// FUNCTIONS FOR MODEL
+var player = {
+    "id": 1,
+    "last_shot": [0,0]
+};
+var computer = {
+    "id": 2,
+    "last_shot": [0,0]
+};
+var playerShots = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+var computerShots = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+var playerShips = [
+    {
+        "name": "carrier",
+        "id": 1,
+        "head_location": null,
+        "length": 5,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    },
+    {
+        "name": "battleship",
+        "id": 2,
+        "head_location": null,
+        "length": 4,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    },
+    {
+        "name": "cruiser",
+        "id": 3,
+        "head_location": null,
+        "length": 3,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    },
+    {
+        "name": "submarine",
+        "id": 4,
+        "head_location": null,
+        "length": 3,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    },
+    {
+        "name": "destroyer",
+        "id": 5,
+        "head_location": null,
+        "length": 2,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    }
+];
+var computerShips = [
+    {
+        "name": "carrier",
+        "id": 1,
+        "head_location": null,
+        "length": 5,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    },
+    {
+        "name": "battleship",
+        "id": 2,
+        "head_location": null,
+        "length": 4,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    },
+    {
+        "name": "cruiser",
+        "id": 3,
+        "head_location": null,
+        "length": 3,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    },
+    {
+        "name": "submarine",
+        "id": 4,
+        "head_location": null,
+        "length": 3,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    },
+    {
+        "name": "destroyer",
+        "id": 5,
+        "head_location": null,
+        "length": 2,
+        "orientation": null,
+        "hits": 0,
+        "sunk": false
+    }
+];
+var playerShipPlacement = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+var computerShipPlacement = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+var gameOver = false;
+var turn = 1;
+
+// SHOT function
+// returns 1 on a hit and 0 on a miss
+function shot(id, row, col) {
+    // determine which player it is
+    if (id == 1) {
+        // players turn, get players info
+        if (playerShots[row][col] == 1) {
+            alert("Already shot here, try again.");
+        }
+        else {
+            if (computerShipPlacement[row][col] != 0) {
+                playerShots[row][col] = 1;
+                var ship = findShip(computerShips, computerShipPlacement[row][col]);
+                ship.hits++;
+                if (ship.hits == ship.length) {
+                    alert("The enemies " + ship.name + " has been sunk!");
+                    return 2;
+                }
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+    else {
+        // computers turn
+        if (computerShots[row][col] == 1) {
+            computerCalcShot();
+        }
+        else {
+            if (playerShipPlacement[row][col] != 0) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+}
+
+// COMPUTERCALCSHOT function
+// calls the shot function on a random index
+function computerCalcShot() {
+    var row = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var col = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var r = Math.floor(Math.random() * 10);
+    var c = Math.floor(Math.random() * 10);
+    shot(2, r, c);
+}
+
+// PLACESHIPS
+// places the ships for the player and computer (just a testing function)
+function placeShipsPlayer() {
+    // place the carrier (5)
+    var pCarrier = findShip(playerShips, 1);
+    pCarrier.head_location = [0,0];
+    pCarrier.orientation = "vertical";
+    placeShip(1, pCarrier);
+    // place the battleship (4)
+    var pBattleship = findShip(playerShips, 2);
+    pBattleship.head_location = [0,1];
+    pBattleship.orientation = "vertical";
+    placeShip(1, pBattleship);
+    // place cruiser (3)
+    var pCruiser = findShip(playerShips, 3);
+    pCruiser.head_location = [0,2];
+    pCruiser.orientation = "vertical";
+    placeShip(1, pCruiser);
+    // place submarine (3)
+    var pSubmarine = findShip(playerShips, 4);
+    pSubmarine.head_location = [0,3];
+    pSubmarine.orientation = "vertical";
+    placeShip(1, pSubmarine);
+    // place destroyer (2)
+    var pDestroyer = findShip(playerShips, 5);
+    pDestroyer.head_location = [0,4];
+    pDestroyer.orientation = "vertical";
+    placeShip(1, pDestroyer);
+
+    return playerShipPlacement;
+}
+function placeShipsComputer() {
+    // just copy player for now
+    computerShipPlacement = playerShipPlacement;
+    return computerShipPlacement;
+}
+
+function placeShip(id, ship) {
+    var orientation = ship.orientation;
+    var len = ship.length;
+    var row = ship.head_location[0];
+    var col = ship.head_location[1];
+    
+    if (id == 1) {
+        if (orientation == "vertical") {
+            for (var i = 0; i < len; i++) {
+                playerShipPlacement[row + i][col] = ship.id;
+            }
+        }
+        else {
+            for (var i = 0; i < len; i++) {
+                playerShipPlacement[row][col + i] = ship.id;
+            }
+        }
+    }
+    else {
+        if (orientation == "vertical") {
+            for (var i = 0; i < len; ++i) {
+                computerShipPlacement[row + i][col + i] = ship.id;
+            }
+        }
+        else {
+            for (var i = 0; i < len; ++i) {
+                computerShipPlacement[row][col + i] = ship.id;
+            }
+        }
+    }
+}
+
+function findShip(objs, id) {
+    
+    for (var i = 0; i < objs.length; i++) {
+        if (objs[i]["id"] === id) {
+            return objs[i];
+        }
+    }
+    return null;
+}
+
+function reset() {
+    player = {
+        "id": 1,
+        "last_shot": [0,0]
+    };
+    computer = {
+        "id": 2,
+        "last_shot": [0,0]
+    };
+    playerShots = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    computerShots = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    playerShips = [
+        {
+            "name": "carrier",
+            "id": 1,
+            "head_location": null,
+            "length": 5,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        },
+        {
+            "name": "battleship",
+            "id": 2,
+            "head_location": null,
+            "length": 4,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        },
+        {
+            "name": "cruiser",
+            "id": 3,
+            "head_location": null,
+            "length": 3,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        },
+        {
+            "name": "submarine",
+            "id": 4,
+            "head_location": null,
+            "length": 3,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        },
+        {
+            "name": "destroyer",
+            "id": 5,
+            "head_location": null,
+            "length": 2,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        }
+    ];
+    computerShips = [
+        {
+            "name": "carrier",
+            "id": 1,
+            "head_location": null,
+            "length": 5,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        },
+        {
+            "name": "battleship",
+            "id": 2,
+            "head_location": null,
+            "length": 4,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        },
+        {
+            "name": "cruiser",
+            "id": 3,
+            "head_location": null,
+            "length": 3,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        },
+        {
+            "name": "submarine",
+            "id": 4,
+            "head_location": null,
+            "length": 3,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        },
+        {
+            "name": "destroyer",
+            "id": 5,
+            "head_location": null,
+            "length": 2,
+            "orientation": null,
+            "hits": 0,
+            "sunk": false
+        }
+    ];
+    playerShipPlacement = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    computerShipPlacement = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    gameOver = false;
+    turn = 1;
+}
